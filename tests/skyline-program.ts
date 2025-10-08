68368248178c36d2bfd9bc8414655f05500efa1c
filip <grujicf998@gmail.com>
2025-10-08 16:15:36 +0200

    Add test for initialization

--- tests/skyline-program.ts
+++ tests/skyline-program.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, web3 } from "@coral-xyz/anchor";
 import { SkylineProgram } from "../target/types/skyline_program";
+import { assert, expect } from "chai";
 
 describe("skyline-program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.skylineProgram as Program<SkylineProgram>;
+  const owner = provider.wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const validators: web3.Keypair[] = [];
+  for (let i = 0; i < 10; i++) {
+    validators.push(anchor.web3.Keypair.generate());
+  }
+
+  const vsPDA = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("validator-set")],
+    program.programId
+  )[0];
+
+  describe("Initialize", () => {
+    it("successfully", async () => {
+      await program.methods
+        .initialize(validators.map((v) => v.publicKey))
+        .accounts({
+          signer: owner.publicKey,
+        })
+        .rpc();
+
+      const vs = await program.account.validatorSet.fetch(vsPDA);
+      vs.signers.forEach((v) => {
+        assert(validators.find((val) => val.publicKey.equals(v)));
+      });
+    });
   });
 });
